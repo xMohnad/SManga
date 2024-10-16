f830751edb2e127691cbc51b666908fe535a98a6
xMohnad <mohnad00967@gmail.com>
2024-10-16 23:53:15 +0000

    Change domain MangaSwet spider

--- SManga/spiders/mangaSwet.py
+++ SManga/spiders/mangaSwet.py
@@ -3,4 +3,4 @@ from SManga.lib.themes.mangathemesia import MangaThemesiaSpider
 
 class MangaswetSpider(MangaThemesiaSpider):
     name = "MangaSwet"
-    base_url = "https://healteer.com/"
+    base_url = "https://swatscans.com/"
