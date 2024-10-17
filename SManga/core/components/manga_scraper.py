0f7861070c02f34eb28324688d7d87ceb1207710
xMohnad <mohnad00967@gmail.com>
2024-10-17 00:15:50 +0000

    - Enhanced `print_spiders` method to display spider language and base URL - Added support for providing a custom `USER_AGENT` for crawling.

--- SManga/core/components/manga_scraper.py
+++ SManga/core/components/manga_scraper.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 from typing import Optional
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy.spiderloader import SpiderLoader
+from scrapy.settings import Settings
+from rich import print 
 
 from SManga.core.components import SpiderDataProcessor
 
@@ -11,49 +14,50 @@ class SManga:
     def __init__(
         self,
         url: Optional[str] = None,
-        file_name: str = "chapters.json",
+        user_agent: Optional[str] = None,
+        file_name: Optional[str | Path] = None,
         overwrite: bool = False,
     ) -> None:
-        """
-        Initializes the SManga class with the given parameters.
-        """
+        """Initializes the SManga class with the given parameters."""
         self.url = url
         self.file_name = file_name
-        self.file_format = self._validate_file_format(file_name)
         self.overwrite = overwrite
+        self.file_format = self._validate_file_format(file_name)
 
-        self.local_settings = self._prepare_local_settings(
-            file_name, self.file_format, overwrite
+        self.process_settings = self._prepare_settings(
+            file_name, self.file_format, overwrite, user_agent
         )
 
-        self.settings = get_project_settings()
-        self.spider_loader = SpiderLoader.from_settings(self.settings)
+        self.spider_loader = SpiderLoader.from_settings(self.process_settings)
         self.list_spiders = self.spider_loader.list()
-
+        
         self.processor = None
         self.spider_name = None
-
-    def _validate_file_format(self, file_name: str) -> str:
+        
+    def _validate_file_format(self, file_name: Path) -> str:
         """Validates the file format based on the extension."""
-        file_format = file_name.rsplit(".", 1)[-1].lower()
+        file_format = str(file_name).rsplit(".", 1)[-1].lower()
+
         if file_format not in SUPPORTED_FORMATS:
             raise TypeError(
                 f"Unsupported file format: {file_format}. Please use one of the supported formats: {', '.join(SUPPORTED_FORMATS)}."
             )
         return file_format
 
-    def _prepare_local_settings(
-        self, file_name: str, file_format: str, overwrite: bool
+    def _prepare_settings(
+        self, file_name: str, file_format: str, overwrite: bool, user_agent: str
     ) -> dict:
-        """Prepares local settings for the crawling process."""
-        return {
+        """Prepares settings for the crawling process."""
+        return Settings({
             "FEEDS": {
                 file_name: {
                     "format": file_format,
                     "overwrite": overwrite,
                 }
-            }
-        }
+            },
+            **get_project_settings(),
+            **({"USER_AGENT": user_agent} if user_agent else {})
+        })
 
     def is_valid_url(self, base_url: str) -> bool:
         """Checks if the provided URL starts with the given base URL."""
@@ -61,17 +65,16 @@ class SManga:
 
     def print_spiders(self) -> None:
         """Prints the names of all available spiders."""
-        print("Available spiders: ")
+        print("[magenta]Available spiders: ")
         for spider_name in self.list_spiders:
-            print(f"  - {spider_name}")
+            spider_cls = self.spider_loader.load(spider_name)
+            print(f" -[cyan] {spider_name.capitalize():<15}[/] [green]{spider_cls.language.capitalize():<5} [/]{spider_cls.base_url}")
 
     def find_spider_by_base_url(self) -> Optional[str]:
         """Finds a spider that matches the base URL of the provided URL."""
         for spider_name in self.list_spiders:
             spider_cls = self.spider_loader.load(spider_name)
-            if hasattr(spider_cls, "base_url") and self.is_valid_url(
-                spider_cls.base_url
-            ):
+            if self.is_valid_url(spider_cls.base_url):
                 return spider_name
         return None
 
@@ -91,9 +94,7 @@ class SManga:
             return self.processor.load_processed_data()
 
     def start(self, spider_name: Optional[str] = None) -> None:
-        """
-        Starts the crawling process using the appropriate spider.
-        """
+        """Starts the crawling process using the appropriate spider."""
         if not self.url:
             raise ValueError("Error: Missing argument 'URL'.")
 
@@ -105,9 +106,8 @@ class SManga:
 
         self.processor = SpiderDataProcessor(spider_name)
         self.spider_name = spider_name
-        process_settings = {**self.settings, **self.local_settings}
 
-        process = CrawlerProcess(process_settings)
+        process = CrawlerProcess(self.process_settings)
         process.crawl(spider_name, url=self.url)
         process.start()
 
