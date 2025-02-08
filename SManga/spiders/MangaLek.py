2df0365da07ee5461357f405afea6e17b337674f
xMohnad <mohnad00967@gmail.com>
2025-02-08 17:47:05 +0000

    fix(spider): Remove incorrect manga name selector in MangaLek
    - Removed incorrect `selector_manganame`, which was overriding the correct selector from the base spider.

--- SManga/spiders/MangaLek.py
+++ SManga/spiders/MangaLek.py
@@ -5,5 +5,4 @@ class MangalekSpider(Madara):
     name = "MangaLek"
     base_url = "https://lekmanga.net/"
     language = "Ara"
-    selector_manganame = ".c-breadcrumb li:nth-child(2) > a::text"
     home_selector = "ol.breadcrumb li:nth-child(2) > a::attr(href)"
